ea7224a4bc94d1b746b32d91834c676bc57ab83f
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-04-06 11:53:51 -0400

    CREATE MODELS

---
+++ models/author.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const authorSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    authoredQuotes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Quote'
+    }],
+    birthDate: Date,
+    bio: String,
+    url: String
+}, {timestamps: true});
+
+module.exports = mongoose.models('Author', authorSchema);
---
+++ models/quote.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// The schema defines the shape of the document
+const quoteSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    genre: [String],
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'Author'
+    },
+}, {timestamps: true});
+
+// Compile the schema into a model, export
+module.exports = mongoose.model('Quote', quoteSchema);
