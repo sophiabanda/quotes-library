4b00f09b29dc66c16e9c9d5a611e88178dfd1671
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-06-07 12:42:47 -0400

    enlarged main page quote font, auto prettier updates

--- routes/index.js
+++ routes/index.js
@@ -3,39 +3,38 @@ const router = express.Router();
 const passport = require('passport');
 const Quote = require('../models/quote');
 
-
 // GET Home Page
-router.get("/", async function(req, res, next) {
-  let quotes = await Quote.find({}).populate("author");
+router.get('/', async function (req, res, next) {
+  let quotes = await Quote.find({}).populate('author');
   let j;
   for (let i = quotes.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1));
     [quotes[i], quotes[j]] = [quotes[j], quotes[i]];
   }
   let quote = quotes[0];
-  res.render("index", { title: "Quote of the Moment", quote });
+  res.render('index', { title: 'Quote of the Moment', quote });
 });
 
 // Google OAuth login route
-router.get('/auth/google', passport.authenticate(
-  'google',
-  {
+router.get(
+  '/auth/google',
+  passport.authenticate('google', {
     scope: ['profile', 'email'],
-  }
-));
+  })
+);
 
 // Google OAuth callback route
-router.get('/oauth2callback', passport.authenticate(
-  'google',
-  {
+router.get(
+  '/oauth2callback',
+  passport.authenticate('google', {
     successRedirect: '/',
-    failureRedirect: '/'
-  }
-));
+    failureRedirect: '/',
+  })
+);
 
 // OAuth logout route
-router.get('/logout', function(req, res){
-  req.logout(function() { 
+router.get('/logout', function (req, res) {
+  req.logout(function () {
     res.redirect('/');
   });
 });
